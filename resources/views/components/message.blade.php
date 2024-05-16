75d8c1e8ab9f43f3dea7132d43565adc3451c60f
Ehsan Davari <ehsndvr81@gmail.com>
2024-05-16 08:08:47 +0330

    fix some css fings

--- resources/views/components/message.blade.php
+++ resources/views/components/message.blade.php
@@ -1,14 +1,6 @@
 <section class="main-massage">
     <div class="main-massage__header">
         <div class="massage-header__info">
-            <div class="massage-header__back">
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#f6f6f6"
-                    class="bi bi-arrow-return-left" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd"
-                        d="M14.5 1.5a.5.5 0 0 1 .5.5v4.8a2.5 2.5 0 0 1-2.5 2.5H2.707l3.347 3.346a.5.5 0 0 1-.708.708l-4.2-4.2a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 8.3H12.5A1.5 1.5 0 0 0 14 6.8V2a.5.5 0 0 1 .5-.5" />
-                </svg>
-            </div>
-
             <div class="box">
                 <h3 class="user-name">Username Group</h3>
                 <p class="user-last__seen">50 member, 20 online</p>
@@ -25,11 +17,6 @@
                 viewBox="0 0 16 16">
                 <path
                     d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.6 17.6 0 0 0 4.168 6.608 17.6 17.6 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.68.68 0 0 0-.58-.122l-2.19.547a1.75 1.75 0 0 1-1.657-.459L5.482 8.062a1.75 1.75 0 0 1-.46-1.657l.548-2.19a.68.68 0 0 0-.122-.58zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z" />
-            </svg>
-            <svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" fill="#7f8c8d"
-                class="bi bi-layout-sidebar-reverse" viewBox="0 0 16 16">
-                <path
-                    d="M16 3a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2zm-5-1v12H2a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1zm1 0h2a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1h-2z" />
             </svg>
             <svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" fill="#7f8c8d"
                 class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
